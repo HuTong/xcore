004b5eddfb3d39dae2f87b7471e53cd48730bb06
hutong <hutong0521@163.com>
2017-03-03 09:46:54 +0800

    记录球半径计算

--- Tool.php
+++ Tool.php
@@ -27,4 +27,24 @@ class Tool
 	        return $uuid;
 	    }
 	}
+
+	/** 
+	*  @desc 根据两点间的经纬度计算距离 
+	*        借助 composer require geohash/geohash 可以查找附近的事物
+	*  @param float $latitude 纬度值 
+	*  @param float $longitude 经度值 
+	*/  
+	function getDistance($latitude1, $longitude1, $latitude2, $longitude2)   
+	{  
+	    $earth_radius = 6371000;   //半径
+	      
+	    $dLat = deg2rad($latitude2 - $latitude1);  
+	    $dLon = deg2rad($longitude2 - $longitude1);  
+
+	    $a = sin($dLat/2) * sin($dLat/2) + cos(deg2rad($latitude1)) * cos(deg2rad($latitude2)) * sin($dLon/2) * sin($dLon/2);  
+	    $c = 2 * asin(sqrt($a));  
+	    $d = $earth_radius * $c;  
+	      
+	    return round($d);   //四舍五入  
+	}
 }
